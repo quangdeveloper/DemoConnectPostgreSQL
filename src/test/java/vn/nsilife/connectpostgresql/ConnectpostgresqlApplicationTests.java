cea6c51ec370c44086c303f84bed34cb6da1c2bb
quang <nguyenvanquangk11@gmail.com>
2020-07-29 14:58:47 +0700

    commit on 29/07/2020

---
+++ src/test/java/vn/nsilife/connectpostgresql/ConnectpostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nsilife.connectpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectpostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
